7e70b0f37f0cbffc468880b0c4e3f32e5f72f406
Monota <monota.oss@gmail.com>
2018-03-29 22:26:04 +0900

    Change password encoder.

--- src/main/java/tokyo/monota/work/result/manager/core/security/SecurityConfig.java
+++ src/main/java/tokyo/monota/work/result/manager/core/security/SecurityConfig.java
@@ -7,7 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -32,11 +33,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(authenticationService()).passwordEncoder(NoOpPasswordEncoder.getInstance());
+		auth.userDetailsService(authenticationService()).passwordEncoder(passwordEncoder());
 	}
 
 	@Bean
 	AuthenticationService authenticationService() {
 		return new AuthenticationService();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
