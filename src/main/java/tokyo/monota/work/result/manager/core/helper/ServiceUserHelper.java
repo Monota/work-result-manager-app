c1b92d555f43b0303d423eac78e4a52860a5dadd
Monota <monota.oss@gmail.com>
2018-04-01 01:27:18 +0900

    Add helper method to get current loggedin user id.

---
+++ src/main/java/tokyo/monota/work/result/manager/core/helper/ServiceUserHelper.java
@@ -0,0 +1,6 @@
+package tokyo.monota.work.result.manager.core.helper;
+
+public interface ServiceUserHelper {
+
+	public String getCurrentUserId();
+}
---
+++ src/main/java/tokyo/monota/work/result/manager/core/helper/impl/ServiceUserHelperImpl.java
@@ -0,0 +1,19 @@
+package tokyo.monota.work.result.manager.core.helper.impl;
+
+import java.security.Principal;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+import tokyo.monota.work.result.manager.core.helper.ServiceUserHelper;
+
+public class ServiceUserHelperImpl implements ServiceUserHelper {
+
+	public String getCurrentUserId() {
+		Principal principal = SecurityContextHolder.getContext().getAuthentication();
+		if (principal instanceof User) {
+			return ((User) principal).getUsername();
+		}
+		return null;
+	}
+}
