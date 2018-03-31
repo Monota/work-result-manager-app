c8aecc10e539d2452c87dbaaed8753bfb13bd7d2
Monota <monota.oss@gmail.com>
2018-04-01 01:33:39 +0900

    Fix to use loggedin user id.

--- src/main/java/tokyo/monota/work/result/manager/domain/service/impl/WorkServiceImpl.java
+++ src/main/java/tokyo/monota/work/result/manager/domain/service/impl/WorkServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import tokyo.monota.work.result.manager.core.helper.ServiceUserHelper;
 import tokyo.monota.work.result.manager.domain.entity.WorkItemEntity;
 import tokyo.monota.work.result.manager.domain.mapper.ItemMasterMapper;
 import tokyo.monota.work.result.manager.domain.mapper.WorkItemMapper;
@@ -27,93 +28,79 @@ public class WorkServiceImpl implements WorkService {
 	@Autowired
 	ItemMasterMapper itemMasterMapper;
 
+	@Autowired
+	ServiceUserHelper serviceUserHelper;
+
 	@Override
 	public List<WorkResource> getWorkItemByWorkMonth(String currentWorkMonth) {
-
 		List<WorkItemEntity> workItemEntityList = workItemMapper.selectWorkItemsByWorkMonth(currentWorkMonth);
-
 		List<WorkResource> workResourceList = new ArrayList<>();
 		for (WorkItemEntity entity : workItemEntityList) {
 			WorkResource resource = new WorkResource();
 			BeanUtils.copyProperties(entity, resource);
 			workResourceList.add(resource);
 		}
-
 		return workResourceList;
 	}
 
 	@Override
 	public MonthListResource getAllWorkMonths(String currentWorkMonth) {
-
 		List<String> workMonthList = workItemMapper.selectWorkMonths();
-
 		MonthListResource monthList = new MonthListResource();
 		for (String workMonth : workMonthList) {
 			monthList.addValue(new MonthSelectionResource(workMonth, workMonth, workMonth.equals(currentWorkMonth)));
 		}
-
 		return monthList;
 	}
 
 	@Override
 	public List<String> getAllItemTypeNames() {
-
 		return itemMasterMapper.selectAllItemTypeNames();
 	}
 
 	@Override
 	public String getItemUnitPrice(String itemTypeName, Boolean itemIsNew) {
-
 		BigDecimal unitPrice = itemMasterMapper.selectUnitPrice(itemTypeName, itemIsNew);
-
 		if (unitPrice == null) {
 			return "";
 		}
-
 		unitPrice = unitPrice.setScale(2, RoundingMode.FLOOR);
-
 		return unitPrice.toString();
 	}
 
 	@Override
 	public String getTotalPrice(String currentWorkMonth) {
-
 		BigDecimal totalPrice = workItemMapper.sumupUnitPrice(currentWorkMonth);
-
 		if (totalPrice == null) {
 			return "";
 		}
-
 		totalPrice = totalPrice.setScale(2, RoundingMode.FLOOR);
-
 		return totalPrice.toString();
 	}
 
 	@Override
 	@Transactional
 	public void updateWorkItem(WorkResource resource) {
-
-		WorkItemEntity entity = workItemMapper.selectByPkForUpdate("user", resource.getWorkDate(), resource.getItemTypeName(), resource.getItemIsNew());
-
+		String userId = serviceUserHelper.getCurrentUserId();
+		WorkItemEntity entity = workItemMapper.selectByPkForUpdate(userId, resource.getWorkDate(), resource.getItemTypeName(), resource.getItemIsNew());
 		if (entity == null) {
 			throw new IllegalStateException("entity is not found.");
 		}
-
 		entity.setItemQuantity(resource.getItemQuantity());
 		workItemMapper.updateWorkItem(entity);
 	}
 
 	@Override
 	public void deleteWorkItem(WorkResource resource) {
-
-		workItemMapper.deleteWorkItem("user", resource.getWorkDate(), resource.getItemTypeName(), resource.getItemIsNew());
+		String userId = serviceUserHelper.getCurrentUserId();
+		workItemMapper.deleteWorkItem(userId, resource.getWorkDate(), resource.getItemTypeName(), resource.getItemIsNew());
 	}
 
 	@Override
 	public void createWorkItem(WorkResource resource) {
-
+		String userId = serviceUserHelper.getCurrentUserId();
 		WorkItemEntity entity = new WorkItemEntity();
-		entity.setUserId("user");
+		entity.setUserId(userId);
 		entity.setWorkDate(resource.getWorkDate());
 		entity.setItemIsNew(resource.getItemIsNew());
 		entity.setItemTypeName(resource.getItemTypeName());
