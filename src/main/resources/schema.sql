47bb447e5218398b5bda68e8fb64367461cb22e4
Monota <monota.oss@gmail.com>
2018-04-01 01:10:08 +0900

    Fix ddl.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,7 +13,7 @@ create table if not exists work_item (
     primary key (user_id, work_date, item_type_name, item_is_new)
 );
 
-create table item_master (
+create table if not exists item_master (
     item_type_name varchar(100) not null,
     item_is_new boolean not null,
     item_unit_price decimal(5, 2) not null,
